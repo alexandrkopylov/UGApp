5efdae24e68e6945fab914a18c4facdd2e651dd0
alexandrkopylov <5186441@gmail.com>
2016-01-25 12:08:07 +0300

    123

--- server.js
+++ server.js
@@ -37,7 +37,17 @@ app.get('/logs',function(req,res){
 	logdb('Get /logs');
 	db_log.find();
 	res.send('Get /logs');
-})
+});
+app.get("/test",function(req,res){
+    var data = [];
+    client.HGETALL("receipts",function(err,obj){
+        for(var id in obj){
+            data.push(JSON.parse(obj[id]));
+        }
+        console.log(data);
+        res.json(data);
+    });
+});
 
 app.listen(80, function () {
     console.log('Port 80');
