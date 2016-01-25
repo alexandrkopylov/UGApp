6707ea1485c73e9a3c1ed2001d015e0ef4630af1
alexandrkopylov <5186441@gmail.com>
2016-01-25 23:46:40 +0300

    add post to lic

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ app.get('/', function (req, res) {
 app.get('/lic', function (req, res) {
     res.send('UGApp Server lic port');
 });
+app.post('/lic', function (req, res) {
+    res.send(1);
+});
 
 app.listen(80, function () {
     console.log('Port 80');
