7248a896eacb9eb2c9c80fe30a2b235c973c519e
alexandrkopylov <5186441@gmail.com>
2016-01-25 23:28:17 +0300

    add lic get

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('UGApp Server started');
+    res.send('UGApp Server started<br>');
+});
+app.get('/lic', function (req, res) {
+    res.send('UGApp Server lic port');
 });
 
 app.listen(80, function () {
-    console.log('Port 3000');
+    console.log('Port 80');
 });
 // JavaScript source code
