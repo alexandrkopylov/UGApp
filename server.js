b1e91fce0cfe5943274c1f42c2f26a43a326bcc4
alexandrkopylov <5186441@gmail.com>
2016-01-26 01:14:10 +0300

    connect to DB

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+    console.log('Connected to DB');
+});
+var LicT01 = mongoose.model('Lic', { name: Sring });
+var nlic = new LicT01({ name: '!QW134' });
+nlic.save(function (err) {
+    if (err)
+        console.log('ER0001');
+})
 
 app.get('/', function (req, res) {
     res.send('UGApp Server started<br>');
