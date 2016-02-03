6cd2b5718eba1c1f1e586cf070ed620ef0e870b8
alexandrkopylov <5186441@gmail.com>
2016-02-03 12:49:11 +0300

    Change API routes for Category

---
+++ routes/api_route_categories.js
@@ -0,0 +1,61 @@
+#!/usr/bin/env node
+var express=require('express');
+var router = express.Router();
+var log=require('../libs/log')(module);
+
+var CategoryModel = require('../libs/mongoose').CategoryModel;
+
+router.get('/', function (req, res) {
+    log.info('GET API categories')
+    return CategoryModel.find(function (err, categories) {
+        if (!err) {
+            return res.send(categories);
+        } else {
+            res.statusCode = 500;
+            log.error('Internal error (%d): %s', res.statusCode, err.message);
+            return res.send({error:'Server error'});
+        }
+        
+    });
+});
+
+router.post('/', function(req,res){
+    log.info ('POST API categories');
+    var category = new CategoryModel({
+        title: req.body.title,
+        author: req.body.author,
+        visible: req.body.visible
+    });
+
+    category.save(function(err){
+    if (!err){
+        log.info("Added category");
+        return res.send({status:'OK', category:category});
+    } else {
+    log.error(err.message);
+        console.log(err);
+        if(err.name == 'ValidationError') {
+            res.statusCode = 400;
+            res.send({ error: 'Validation error' });
+        } else {
+            res.statusCode = 500;
+            res.send({ error: 'Server error' });
+        }
+        log.error('Internal error (%d): %s', res.statusCode, err.message);
+        }
+});
+});
+
+router.get('/:id', function(req,res){
+   log.info('Get API Category by :id');
+});
+
+router.put('/:id', function (req,res){
+    log.info('Put API Category by :id');
+});
+
+router.delete('/api/categories/:id', function(req,res){
+    log.info('Delete API Category by :id');
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var api_category_route=require('./routes/api_route_categories');
 var log=require('./libs/log')(module);
 var config=require('./libs/config');
 
-var CategoryModel = require('./libs/mongoose').CategoryModel;
+
 
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
 
 app.get('/', function (req, res) {
     log.info('Get /');
@@ -24,58 +24,7 @@ app.post('/lic', function (req, res) {
 	res.send('1');
 });
 
-app.get('/api/categories', function (req, res) {
-    return CategoryModel.find(function (err, categories) {
-        if (!err) {
-            return res.send(categories);
-        } else {
-            res.statusCode = 500;
-            log.error('Internal error (%d): %s', res.statusCode, err.message);
-            return res.send({error:'Server error'});
-        }
-        
-    });
-});
-
-app.post('/api/categories', function(req,res){
-    log.info ('POST /api/categories');
-    var category = new CategoryModel({
-        title: req.body.title,
-        author: req.body.author,
-        visible: req.body.visible
-    });
-
-    category.save(function(err){
-    if (!err){
-        log.info("Added category");
-        return res.send({status:'OK', category:category});
-    } else {
-    log.error(err.message);
-        console.log(err);
-        if(err.name == 'ValidationError') {
-            res.statusCode = 400;
-            res.send({ error: 'Validation error' });
-        } else {
-            res.statusCode = 500;
-            res.send({ error: 'Server error' });
-        }
-        log.error('Internal error (%d): %s', res.statusCode, err.message);
-        }
-});
-});
-
-app.get('/api/categories/:id', function(req,res){
-   log.info('Get /api/categories/:id');
-});
-
-app.put('/api/categories/:is', function (req,res){
-    log.info('Put /api/categories/:id');
-});
-
-app.delete('/api/categories/:id', function(req,res){
-    log.info('Delete /api/categories/:id');
-});
-
+app.use('/api/categories',api_category_route);
 
 app.listen(config.get('port'), function () {
     log.info('Server listening on port'+config.get('port'));
