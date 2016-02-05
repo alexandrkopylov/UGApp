3fc1ef785eade1bab1c5b05f25dc9ddbc0dca353
alexandrkopylov <5186441@gmail.com>
2016-02-05 16:32:32 +0300

    !t

--- libs/update_currency_exch.js
+++ libs/update_currency_exch.js
@@ -1,8 +1,40 @@
 #!/usr/bin/env node
 var log=require('./log')(module);
+var CurrencyModel = require('../libs/mongoose').CurrencyModel;
+var CurrencyExchModel = require ('../libs/mongoose').CurrencyExchModel;
+
 var update_currency_exch = function () {
     log.info('Starting Update Currency Exch');
-    return 0;
+    var currincies=CurrencyModel.find({});
+    
+    return currincies;
+    /*
+    return CurrencyModel.find( function (err,currency){
+       if (!currency){
+        res.statusCode = 404;
+        return res.send ({ error: 'Not found'});
+        }
+        currency.exch_to_RUB.push({
+            exch: 0
+        });
+                
+        return currency.save(function (err){
+            if (!err) {
+                log.info('Currency Exch Updated');
+                return res.send({status:'OK', currency:currency.exch_to_RUB});
+            } else {
+            if(err.name == 'ValidationError') {
+                    res.statusCode = 400;
+                    res.send({ error: 'Validation error' });
+                } else {
+                    res.statusCode = 500;
+                    res.send({ error: 'Server error' });
+                }
+                log.error('Internal error(%d): %s',res.statusCode,err.message);    
+            }
+        })
+    });*/
+
 };
 
 module.exports = update_currency_exch;
