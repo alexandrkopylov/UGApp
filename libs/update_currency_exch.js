9a37fc9eb0ce3d53879e14799c02b1d55cefd875
alexandrkopylov <5186441@gmail.com>
2016-02-05 16:20:32 +0300

    !t

--- libs/update_currency_exch.js
+++ libs/update_currency_exch.js
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
 var log=require('./log')(module);
-function update_currency_exch() {
+var update_currency_exch = function () {
     log.info('Starting Update Currency Exch');
     return 0;
-}
+};
+
 module.exports = update_currency_exch;
