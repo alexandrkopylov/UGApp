6e839df3e9acd004b1658cdc3d8bb271dcc046f5
alexandrkopylov <5186441@gmail.com>
2016-02-05 16:07:03 +0300

    !t

--- libs/update_currency_exch.js
+++ libs/update_currency_exch.js
@@ -2,6 +2,6 @@
 var log=require('./log')(module);
 function update_currency_exch() {
     log.info('Starting Update Currency Exch');
-    
+    return 0;
 }
-module.exports = update_currency_exch
+module.exports = update_currency_exch;
