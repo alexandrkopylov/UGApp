bf2afe67c5039ad4575df8eb7479c2b41b30e9ba
alexandrkopylov <5186441@gmail.com>
2016-02-05 12:03:19 +0300

    Add Currency Model, Modified Expense Model

--- libs/mongoose.js
+++ libs/mongoose.js
@@ -33,6 +33,7 @@ var CategoryModel = mongoose.model('Category', Category);
 var Expense=new Schema({
    value:{type: Number, default: 0},
    account_id: {type: ObjectId, require:true},
+   user_id:{type:ObjectId, require:true},
    category_id: {type: ObjectId, require:false},
    currency_id:{type:ObjectId, require:true},
    date:{type: Date, default: Date.now}
@@ -40,5 +41,15 @@ var Expense=new Schema({
 
 var ExpenseModel = mongoose.model('Expense', Expense);
 
+var Currency=new Schema({
+   name:{type:String},
+   short_name:{type:String, require:true},
+   cource_to_RUB:[{date: {type: Date, default:Date.now}, cource:{type:Number,require:true}}],
+   modified:{type:Date, default:Date.now}
+   });
+
+var CurrencyModel = mongoose.model('Currency', Currency);
+
 module.exports.CategoryModel = CategoryModel;
 module.exports.ExpenseModel = ExpenseModel;
+module.exports.CurrencyModel = CurrencyModel;
